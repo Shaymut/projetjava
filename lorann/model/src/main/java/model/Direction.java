3c759e741bb2c322170f98e92dcfc5f829cfcafd
CESI Exia <CESI Exia@Thomas-VAIO>
2018-05-31 14:21:33 +0200

    a

--- lorann/model/src/main/java/model/Direction.java
+++ lorann/model/src/main/java/model/Direction.java
@@ -2,4 +2,13 @@ package model;
 
 public enum Direction {
 
+	UP, 
+	DOWN,
+	LEFT,
+	RIGHT,
+	UP_LEFT,
+	UP_RIGHT,
+	DOWN_LEFT,
+	DOWN_RIGHT;
+	
 }
--- lorann/model/src/main/java/model/Mobile.java
+++ lorann/model/src/main/java/model/Mobile.java
@@ -1,5 +1,10 @@
 package model;
 
-public class Mobile {
+public class Mobile extends Element implements IMobile{
+
+	Mobile(Permeability permeability, char sprite, String image, Position position) {
+		super(permeability, sprite, image, position);
+		// TODO Auto-generated constructor stub
+	}
 
 }
