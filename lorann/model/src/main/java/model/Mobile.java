18cf75257ff07b737597871ac5def89a84dbd2ae
CESI Exia <CESI Exia@Thomas-VAIO>
2018-05-31 14:07:33 +0200

    Création de Mobile, IMobile, Direction

---
+++ lorann/model/src/main/java/model/Mobile.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Mobile extends Element implements IMobile{
+
+	Mobile(Permeability permeability, char sprite, String image) {
+		super(permeability, sprite, image);
+		// TODO Auto-generated constructor stub
+	}
+
+}
