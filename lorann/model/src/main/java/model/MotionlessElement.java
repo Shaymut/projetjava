8ce54259a1f9224f697d40cd4722f6a9330667c8
Stephen <Stephen@Stephen-ROG>
2018-05-31 13:59:13 +0200

    a

---
+++ lorann/model/src/main/java/model/MotionlessElement.java
@@ -0,0 +1,9 @@
+package model;
+
+public abstract class MotionlessElement extends Element{
+
+	MotionlessElement(Permeability permeability, char sprite, String image) {
+		super(permeability, sprite, image);
+	}
+	
+}
