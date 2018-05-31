a0406aa7c71f9c310d623e32b5612ea0e71f5931
Stephen <Stephen@Stephen-ROG>
2018-05-31 14:07:04 +0200

    Position

--- lorann/model/src/main/java/model/Position.java
+++ lorann/model/src/main/java/model/Position.java
@@ -2,5 +2,33 @@ package model;
 
 public class Position {
 	
+	int x;
+	int y;
+	
+	Position(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	Position(Position position){
+		this.x = position.x;
+		this.y = position.y;
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	public int getY() {
+		return this.y;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
 	
 }
