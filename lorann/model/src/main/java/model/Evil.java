57a67519601238a99cceb1ff74b9f03fa7a38a4f
CESI Exia <CESI Exia@Thomas-VAIO>
2018-05-31 15:38:04 +0200

    Evil + Mobile

---
+++ lorann/model/src/main/java/model/Evil.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Evil extends Mobile implements EvilMove, Kill{
+
+	Evil(Permeability permeability, char sprite, String image, Position position) {
+		super(permeability, sprite, image, position);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- lorann/model/src/main/java/model/Mobile.java
+++ lorann/model/src/main/java/model/Mobile.java
@@ -2,9 +2,78 @@ package model;
 
 public class Mobile extends Element implements IMobile{
 
+	private boolean alive = true;
+	
+	private static Position X;
+	
+	private static Position Y;
+	
+	// private IBoard board;
+
 	Mobile(Permeability permeability, char sprite, String image, Position position) {
 		super(permeability, sprite, image, position);
 		// TODO Auto-generated constructor stub
 	}
 
+	public static Position getX() {
+		return X;
+	}
+
+	public void setX(Position x) {
+		X = x;
+	}
+	
+	public static Position getY() {
+		return Y;
+	}
+
+	public void setY(Position y) {
+		Y = y;
+	}
+
+	public boolean isAlive() {
+		return alive;
+	}
+
+	public void setAlive(boolean alive) {
+		this.alive = alive;
+	}
+	
+	public void moveUp() {
+		
+	}
+	
+	public void moveDown() {
+			
+		}
+	
+	public void moveLeft() {
+		
+	}
+	
+	public void moveRight() {
+		
+	}
+
+	public void moveUpLeft() {
+		
+	}
+	
+	public void moveUpRight() {
+		
+	}
+	
+	public void moveDownLeft() {
+		
+	}
+	
+	public void moveDownRight() {
+		
+	}
+	
+	
+	
+	
+	
+	
 }
