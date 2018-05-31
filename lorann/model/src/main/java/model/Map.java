0ac864eca4c7adfe8a99a4f680ad3e6c6da4f003
Stephen <Stephen@Stephen-ROG>
2018-05-31 15:34:09 +0200

    Map

--- lorann/model/src/main/java/model/Map.java
+++ lorann/model/src/main/java/model/Map.java
@@ -1,12 +1,26 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Map {
 	
 	Element[][] map;
 	
 	MotionlessElementFactory motionlessElementFactory;
 	
-	public void CreateMotionlessElement() {
+	public void CreateMap(ArrayList<Tile> ListTiles) {
+		for (Tile tile : ListTiles) {
+			motionlessElementFactory.createMotionlessElement(tile.getSprite(), new Position(tile.getX(),tile.getY()));
+			
+		}
+		
+		
+		/*
+		 * for (final Tile tile : ListTiles) {
+            message.append(tile);
+            message.append('\n');
+        }
+		 */
 	}
 	
 	
