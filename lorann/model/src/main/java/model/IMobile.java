29a69cfb5ac5a2fc89e771fb0bdcfdaa4840174d
Stephen <Stephen@Stephen-ROG>
2018-06-01 23:27:02 +0200

    modification IMobile et Mobile

--- lorann/model/src/main/java/model/IMobile.java
+++ lorann/model/src/main/java/model/IMobile.java
@@ -1,5 +1,20 @@
 package model;
 
 public interface IMobile {
-
+	
+	void moveUp();
+	
+	void moveDown();
+	
+	void moveLeft();
+	
+	void moveRight();
+	
+	void moveUpLeft();
+	
+	void moveUpRight();
+	
+	void moveDownLeft();
+	
+	void moveDownRight();
 }
--- lorann/model/src/main/java/model/Mobile.java
+++ lorann/model/src/main/java/model/Mobile.java
@@ -24,49 +24,57 @@ public class Mobile extends Element implements IMobile{
 	public void setNextX(int nextX) {
 		this.nextX = nextX;
 	}
-
+	
+	@Override
     public void moveUp() {
     	  setNextY(position.getY()-1); //On Sauvegarde l'ancienne position dans une variable temporaire afin de sauvegardé et ensuite 
           //Mettre les méthodes de collision
           position.setY(nextY);
     }
 
+	@Override
     public void moveDown() {
     	setNextY(position.getY()+1);
     	position.setY(nextY);
         }
-
+	
+	@Override
     public void moveLeft() {
     	setNextX(position.getX()-1);
     	position.setX(nextX);
     }
-
+	
+	@Override
     public void moveRight() {
     	setNextX(position.getX()+1);
     	position.setX(nextX);
     }
-
+	
+	@Override
     public void moveUpLeft() {
     	setNextY(position.getY()-1);
     	setNextX(position.getX()-1);
     	position.setX(nextX);
         position.setY(nextY);
     }
-
+	
+	@Override
     public void moveUpRight() {
-    	 setNextY(position.getY()-1); 
-         setNextX(position.getX()+1);
+    	setNextY(position.getY()-1); 
+        setNextX(position.getX()+1);
      	position.setX(nextX);
      	position.setY(nextY);
     }
-
+	
+	@Override
     public void moveDownLeft() {
     	setNextY(position.getY()+1);
     	setNextX(position.getX()-1);
     	position.setX(nextX);
     	position.setY(nextY);
     }
-
+	
+	@Override
     public void moveDownRight() {
     	setNextY(position.getY()+1);
     	setNextX(position.getX()+1);
