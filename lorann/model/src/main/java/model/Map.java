de3bcdb1ff75c4afd71a9c8e0782280c2f99b7aa
CESI Exia <CESI Exia@Thomas-VAIO>
2018-06-05 21:54:21 +0200

    Javadoc de map

--- lorann/model/src/main/java/model/Map.java
+++ lorann/model/src/main/java/model/Map.java
@@ -5,12 +5,14 @@ import java.util.List;
 public class Map {
 	
 	/**
+	 * Creation of the map
 	 * 
 	 * @see Map#CreateMap(List)
 	 */
 	static Element[][] mapNiveau = new Element[20][12];
 	
 	/**
+	 * Creation of motionless elements
 	 * 
 	 * @see Map#CreateMap(List)
 	 */
@@ -18,37 +20,49 @@ public class Map {
 	
 	/**
 	 * 
+	 * Creation of lorann
+	 * 
 	 * @see Map#CreateMap(List)
 	 */
 	Lorann lorann;
 	
 	/**
+	 * Creation of evilRandom
 	 * 
 	 * @see Map#CreateMap(List)
 	 */
 	EvilRandom evilRandom;
 	/**
+	 * Creation of evilHorizontal
 	 * 
 	 * @see Map#CreateMap(List)
 	 */
 	EvilHorizontal evilHorizontal;
 	/**
+	 * Creation of evilVertical
 	 * 
 	 * @see Map#CreateMap(List)
 	 */
 	EvilVertical evilVertical;
 	/**
+	 * Creation of evilPingPong
 	 * 
 	 * @see Map#CreateMap(List)
 	 */
 	EvilPingPong evilPingPong;
 	/**
+	 * Creation of spell
 	 * 
 	 * @see Map#CreateMap(List)
 	 */
 	Spell spell;
 	
-
+	/**
+	 * Creation of the map with images
+	 * 
+	 * @param ListTiles
+	 */
+	
 	public void CreateMap(List<Tile> ListTiles) {
 		for (Tile tile : ListTiles) {
 			if (tile.getSprite() == 'S'||
@@ -79,42 +93,109 @@ public class Map {
 		}
 	}
 	
+	/**
+	 * 
+	 * Get an element with X and Y
+	 * 
+	 * @param {@link Position#getX()}
+	 * @param {@link Position#getY()}
+	 * @return an element
+	 */
+	
 	public Element getElementInMap(int x, int y) {
 		return Map.mapNiveau[x][y];
 	}	
+	
+	/**
+	 * 
+	 * Get a map
+	 * 
+	 * @return a map
+	 */
 	
 	public Element[][] getMap(){
 		return Map.mapNiveau;
 	}
 	
+	/**
+	 * Set a map
+	 * 
+	 * @param map
+	 */
+	
 	public void setMap(Element[][] map){
 		Map.mapNiveau = map;
 	}
+	
+	/**
+	 * 
+	 * Get the sprite of an element in the map
+	 * 
+	 * @param {@link Position#getX()}
+	 * @param {@link Position#getY()}
+	 * @return the sprite of an element
+	 */
 	
 	public char getElementByXY(int x, int y) {
 		return Map.mapNiveau[x][y].getSprite();
 	}
 	
+	/**
+	 * Get lorann
+	 * 
+	 * @return lorann
+	 */
+	
 	public Element getLorann() {
 		return this.lorann;
 	}
+	
+	/**
+	 * Get evilRandom
+	 * 
+	 * @return evilRandom
+	 */
 	
 	public EvilRandom getEvilRandom() {
 		return this.evilRandom;
 	}
 	
+	/**
+	 * Get evilHorizontal
+	 * 
+	 * @return evilHorizontal
+	 */
+	
 	public EvilHorizontal getEvilHorizontal() {
 		return this.evilHorizontal;
 	}
 
+	/**
+	 * Get evilVertical
+	 * 
+	 * @return evilVertical
+	 */
+	
 	public EvilVertical getEvilVertical() {
 		return evilVertical;
 	}
+	
+	/**
+	 * Get evilPingPong
+	 * 
+	 * @return evilPingPong
+	 */
 	
 	public EvilPingPong getEvilPingPong() {
 		return evilPingPong;
 	}
 
+	/**
+	 * Get spell
+	 * 
+	 * @return spell
+	 */
+	
 	public Spell getSpell() {
 		return spell;
 	}
