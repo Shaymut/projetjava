e1c212083f44a1cf56b8e3edd5bc39df4bf88a1e
Stephen <Stephen@Stephen-ROG>
2018-06-05 16:07:37 +0200

    Javadoc -> contract

--- lorann/contract/src/main/java/model/Element.java
+++ lorann/contract/src/main/java/model/Element.java
@@ -3,46 +3,101 @@ package model;
 import java.util.Random;
 
 public class Element {
-	
+	/** The permeability. */
 	Permeability permeability;
+	/** The sprite. */
 	char sprite;
+	/** The position. */
 	Position position;
+	/** The random. */
 	Random random = new Random();
-	
+	/**
+     * Instantiates a new Element.
+     *
+     * @param permeability
+     *            the permeability
+     * @param sprite
+     *            the sprite
+     * @param position
+     *            the position
+     */
 	public Element(Permeability permeability, char sprite, Position position){
 		this.permeability = permeability;
 		this.sprite = sprite;
 		this.position = position;
 	}
 	
+	/**
+     * Gets the id.
+     *
+     * @return the sprite
+     */
 	public char getSprite() {
 		return this.sprite;
 	}
 	
+	/**
+     * Gets the position.x.
+     *
+     * @return the position.x
+     */
 	public  int getX() {
 		return this.position.getX();
 	}
 	
+	/**
+     * Gets the position.y.
+     *
+     * @return the position.y
+     */
 	public int getY() {
 		return this.position.getY();
 	}
 	
+	/**
+     * Sets the x.
+     *
+     * @param x
+     *            the new x
+     */
 	public  void setX(int x) {
 		this.position.x = x;
 	}
 	
+	/**
+     * Gets the position.
+     *
+     * @return the position
+     */
 	public Position getPosition() {
 		return position;
 	}
-
+	
+	/**
+     * Sets the y.
+     *
+     * @param y
+     *            the new y
+     */
 	public void setY(int y) {
 		this.position.y = y;
 	}
 	
+	/**
+     * Sets the position.
+     *
+     * @param position
+     *            the new position
+     */
 	public void setPosition(Position position) {
 		this.position = position;
 	}
-
+	
+	/**
+     * Gets the permeability.
+     *
+     * @return the permeability
+     */
 	public Permeability getPermeability() {
 		return this.permeability;
 	}
--- lorann/contract/src/main/java/model/Position.java
+++ lorann/contract/src/main/java/model/Position.java
@@ -2,31 +2,69 @@ package model;
 
 public class Position {
 	
+	/** The x. */
 	int x;
+	/** The y. */
 	int y;
 	
+	/**
+     * Instantiates a new Position.
+     *
+     * @param x
+     *            the x
+     * @param y
+     *            the y
+     */
 	public Position(int x, int y){
 		this.x = x;
 		this.y = y;
 	}
 	
-	Position(Position position){
+	/**
+     * Instantiates a new Position.
+     *
+     * @param position
+     *            the position
+     */
+	public Position(Position position){
 		this.x = position.x;
 		this.y = position.y;
 	}
 	
+	/**
+     * Gets the x.
+     *
+     * @return the x
+     */
 	public int getX() {
 		return this.x;
 	}
 	
+	/**
+     * Gets the y.
+     *
+     * @return the y
+     */
 	public int getY() {
 		return this.y;
 	}
 	
+	/**
+     * Sets the x.
+     *
+     * @param x
+     *            the new x
+     */
 	public void setX(int x) {
 		this.x = x;
 	}
 	
+	/**
+     * Sets the y.
+     *
+     * @param y
+     *            the new y
+     */
 	public void setY(int y) {
 		this.y = y;
 	}
